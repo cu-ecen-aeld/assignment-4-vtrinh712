5a86a68d1237563565b0ae820290e41feba63e28
vantrinh <vantrinh@Ubuntu.myguest.virtualbox.org>
2023-04-25 11:09:39 -0700

    Push Assignment 4 Part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,21 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    // wait
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    
+    //obtain mutex
+    pthread_mutex_lock(thread_func_args->mutex);
+    
+    //wait
+    usleep(thread_func_args->wait_to_release_ms*1000);
+    
+    //release mutex
+    pthread_mutex_unlock(thread_func_args->mutex);
+    
     return thread_param;
 }
 
@@ -28,6 +42,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+     
+     //allocate memory
+     struct thread_data* thread_param = malloc(sizeof *thread_param);
+     
+     //setup
+     thread_param->mutex = mutex;
+     //wait
+     thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+     //release
+     thread_param->wait_to_release_ms = wait_to_release_ms;
+     
+     //pthread_create return value
+     int rc = pthread_create(thread, NULL, threadfunc, (void*) thread_param);
+     
+     //check return value & return true if thread created successfully
+     if (rc == 0){
+         thread_param->thread_complete_success = true;
+         return thread_param->thread_complete_success;
+     }
+     
+     
     return false;
 }
 
