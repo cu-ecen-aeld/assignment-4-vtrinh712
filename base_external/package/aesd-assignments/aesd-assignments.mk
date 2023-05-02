a16c51560c719b38d0d8e821183827e062e5b3a7
vantrinh <vantrinh@Ubuntu.myguest.virtualbox.org>
2023-05-01 23:25:07 -0700

    Edit .gitmodules file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = '8466ed21d25342e81995fbca40abb5d46588454d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
